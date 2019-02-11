bc99dcd42c0ef6364c9fbc595484f9bc98df7467
EL-S <31227083+EL-S@users.noreply.github.com>
2019-02-11 14:05:46 +1100

    Wrapping and Indication of moves left
    The game now wraps and has a nicer implementation of the history path
    
    The grid size was also increased

--- main.py
+++ main.py
@@ -4,16 +4,16 @@ from random import randint
 #set seed to be the same
 #train a nn to find the best path
 
-grid_width = 10
-grid_height = 10
+grid_width = 20
+grid_height = 20
 
-grid_size = 60
+grid_size = 30
 
 max_val = 6
 
 grid = [[randint(0,max_val) for y in range(grid_height)] for x in range(grid_width)]
 
-history = []
+history = {}
 
 for x in range(grid_width):
         for y in range(grid_height):
@@ -42,8 +42,6 @@ def draw_grid_lines():
         line_width = 1
         pygame.draw.line(surface,grid_colour,start_pos,end_pos,line_width)
 
-running = True
-
 def fill_cell(x,y,colour):
     global grid_size, grid_width, grid_height
     x = x*grid_size
@@ -60,19 +58,17 @@ def fill_grid():
 
     draw_player()
 
-food = 0
-
 def draw_history():
     global history
-    print(history)
-    for location in history:
+    
+    for location, colour in history.items():
         cell_x = location[0]
         cell_y = location[1]
-        colour = (0,0,255)
         fill_cell(cell_x,cell_y,colour)
 
 def draw_player():
-    global player, food, history
+    global player, food, history, grid_width, grid_height
+    
     cell_x = int(player[0])
     cell_y = int(player[1])
 
@@ -83,9 +79,6 @@ def draw_player():
         grid[cell_x][cell_y] = 0
     
     fill_cell(cell_x,cell_y,colour)
-    loc = [int(player[0]),int(player[1])]
-    if loc not in history:
-        history.append(loc)
 
 def draw_food():
     for x in range(grid_width):
@@ -96,9 +89,43 @@ def draw_food():
                 cell_y = y
                 fill_cell(cell_x,cell_y,colour)
 
-moves = 20
+def add_history():
+    global history_colour, player, history
+    history_colour[0] += colour_change
+    history_colour[1] -= colour_change
+    history_colour[2] -= colour_change
+
+    if history_colour[0] < 0:
+        history_colour[0] = 0
+    elif history_colour[0] > 255:
+        history_colour[0] = 255
+    if history_colour[1] < 0:
+        history_colour[1] = 0
+    elif history_colour[1] > 255:
+        history_colour[1] = 255
+    if history_colour[2] < 0:
+        history_colour[2] = 0
+    elif history_colour[2] > 255:
+        history_colour[2] = 255
+
+    colour_val = (history_colour[0],history_colour[1],history_colour[2])
+    
+    loc_pos = (int(player[0]),int(player[1]))
+    loc_colour = colour_val
+
+    history[loc_pos] = loc_colour
+
+food = 0
+
+moves = 30
+
+history_colour = [128,128,128]
+
+colour_change = round(128/moves)
 
 player = [grid_width/2,grid_height/2]
+
+add_history()
 
 fill_grid()
 
@@ -106,12 +133,7 @@ draw_grid_lines()
 
 pygame.display.update()
 
-
-#generate the food
-
-#generate the player
-
-
+running = True
 
 while running:
 
@@ -119,6 +141,7 @@ while running:
     events = pygame.event.get()
     for event in events:
         if event.type == pygame.KEYDOWN:
+            moves_old = moves
             if event.key == pygame.K_w:
                 player[1] -= 1
                 moves -= 1
@@ -131,24 +154,25 @@ while running:
             if event.key == pygame.K_d:
                 player[0] += 1
                 moves -= 1
-            print(moves)
+            if moves_old != moves:
+                if int(player[0]) >= grid_width:
+                    player[0] = 0
+                elif int(player[0]) < 0:
+                    player[0] = grid_width-1
+                if int(player[1]) >= grid_height:
+                    player[1] = 0
+                elif int(player[1]) < 0:
+                    player[1] = grid_height-1
+                add_history()
+            print("Moves Remaining: {}".format(moves))
             if moves <= 0:
                 running = False
     fill_grid()
 
     draw_grid_lines()
-    #move player
-
-    #subtract 1 from moves
-    
-    #check moves remaining
-        #end game if 0
-
-        #print score
-    
     
     pygame.display.update()
-        #draw grid
-        #draw remaining food
-        #draw player
-print(food)
+
+print("Food Collected: {}".format(food))
+
+pygame.quit()
