fb2b5da5e1cfa2fb86212dcb5bdeb0769268190f
EL-S <31227083+EL-S@users.noreply.github.com>
2020-09-08 02:28:51 +1000

    General Improvements and Audio
    Game now has a soundtrack

--- main.py
+++ main.py
@@ -1,40 +1,10 @@
 import pygame
-from random import randint
+from random import seed, randint, getstate, randrange
 import math
 import time
+import sys
 
-#set seed to be the same
 #train a nn to find the best path
-
-pygame.init()
-info = pygame.display.Info() # You have to call this before pygame.display.set_mode()
-screen_width,screen_height = info.current_w,info.current_h
-print(screen_width,screen_height)
-
-grid_size = 40 #automate the process of picking this number so it perfectly fits
-
-grid_height = round(screen_height/grid_size)
-grid_width = round(screen_width/grid_size)
-
-max_val = 15
-
-#grid = [[randint(0,max_val) for y in range(grid_height)] for x in range(grid_width)]
-
-grid = {} #keys are the co ords, values are the type of cell
-
-
-history = {}
-
-for x in range(grid_width): #bad but not that bad
-    for y in range(grid_height):
-        value = randint(0,max_val)
-        if value == max_val:
-            grid[(x,y)] = 1
-
-width = grid_size*grid_width
-height = grid_size*grid_height
-
-surface = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
 
 def draw_grid_lines(): #optimise pls, render this only once
     for x in range(grid_width):
@@ -53,26 +23,20 @@ def draw_grid_lines(): #optimise pls, render this only once
 
 def fill_cell(x,y,colour):
     global grid_size, grid_width, grid_height
-    x = x*grid_size
-    y = y*grid_size
+    x, y = x*grid_size, y*grid_size
     pygame.draw.rect(surface, colour, pygame.Rect(x, y, grid_size, grid_size))
 
 def fill_grid():
-    
     surface.fill((255,255,255))
-
     draw_history()
-    
     draw_food()    
-
     draw_player()
 
 def draw_history():
     global history
     
     for location, colour in history.items():
-        cell_x = location[0]
-        cell_y = location[1]
+        cell_x, cell_y = location
         fill_cell(cell_x,cell_y,colour)
 
 def draw_player():
@@ -95,9 +59,8 @@ def draw_player():
 def draw_food():
     for location, value in grid.items():
         if value == 1:
-            colour = (0,128,0)
-            cell_x = location[0]
-            cell_y = location[1]
+            colour = (randint(0,255),randint(0,255),randint(0,255))
+            cell_x,cell_y = location
             fill_cell(cell_x,cell_y,colour)
 
 def add_history():
@@ -124,32 +87,6 @@ def add_history():
 
     history[loc_pos] = loc_colour
 
-food = 0
-
-food_move_gain = 4
-
-original_moves = 15
-
-moves = original_moves
-
-history_colour = [128.0,128.0,128.0]
-
-colour_change = (128/moves)
-
-player = [round(grid_width/2)-1,round(grid_height/2)-1]
-
-player_old = [0,0]
-
-add_history()
-
-fill_grid()
-
-draw_grid_lines()
-
-pygame.display.update() #first draw
-
-running = True
-
 def wrapping_and_history_update():
     global moves, player, grid_height, grid_width, running
     moves -= 1
@@ -174,34 +111,31 @@ def redraw_scene():
     draw_grid_lines()
     player_rect = [player[0]*grid_size,player[1]*grid_size,grid_size,grid_size]
     player_old_rect = [player_old[0]*grid_size,player_old[1]*grid_size,grid_size,grid_size]
-    pygame.display.update([player_rect,player_old_rect]) #unoptimised yuck
-    #pygame.display.update()
-    time.sleep(0.1)
+    pygame.display.update([player_rect,player_old_rect])
 
-def key_check():
+def key_check(event):
     global moves, player, player_old
-    events = pygame.event.get()
-    #listen for input
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_w]:
+    if event.key == pygame.K_ESCAPE:
+        return True
+    if event.key == pygame.K_w or event.key == pygame.K_UP:
         player_old = (player[0],player[1])
         player[1] -= 1
         wrapping_and_history_update()
     if move_check():
         return
-    if keys[pygame.K_a]:
+    if event.key == pygame.K_a or event.key == pygame.K_LEFT:
         player_old = (player[0],player[1])
         player[0] -= 1
         wrapping_and_history_update()
     if move_check():
         return
-    if keys[pygame.K_s]:
+    if event.key == pygame.K_s or event.key == pygame.K_DOWN:
         player_old = (player[0],player[1])
         player[1] += 1
         wrapping_and_history_update()
     if move_check():
         return
-    if keys[pygame.K_d]:
+    if event.key == pygame.K_d or event.key == pygame.K_RIGHT:
         player_old = (player[0],player[1])
         player[0] += 1
         wrapping_and_history_update()
@@ -211,11 +145,132 @@ def key_check():
 def move_check():
     global moves, running
     if moves <= 0:
-        running = False
+        init(82)
         return True
 
+def game_over():
+    for location, value in grid.items():
+        if value:
+            break
+    else:
+        return True
+
+def choose_move():
+    global moves, player, player_old, grid_width, grid_height
+    nearest_food = None
+    for location, value in grid.items():
+        if value == 1:
+            if location[0] < player[0]:
+                direction_x = -1
+                x_distance = location[0]-player[0]
+            else:
+                direction_x = 1
+                x_distance = location[0]-player[0]
+            if location[1] < player[1]:
+                direction_y = -1
+                y_distance = location[1]-player[1]
+            else:
+                direction_y = 1
+                y_distance = location[1]-player[1]
+            if abs(x_distance) > int(grid_width/2):
+                direction_x *= -1
+                x_distance = -1*(grid_width - x_distance)
+            if abs(y_distance) > int(grid_height/2):
+                direction_y *= -1
+                y_distance = -1*(grid_height - y_distance)
+            abs_x =abs(x_distance)
+            abs_y =abs(y_distance)
+            total_distance = abs_x+abs_y
+            if not nearest_food:
+                nearest_food = [total_distance,x_distance,y_distance,abs_x,abs_y,direction_x,direction_y,location]
+            else:
+                if total_distance < nearest_food[0]:
+                    nearest_food = [total_distance,x_distance,y_distance,abs_x,abs_y,direction_x,direction_y,location]
+    if nearest_food:
+        if nearest_food[3] > 0 and (nearest_food[3] < nearest_food[4] or nearest_food[4] == 0):
+            if nearest_food[-3] < 0:
+                player_old = (player[0],player[1])
+                player[0] -= 1 # left
+            elif nearest_food[-3] > 0:
+                player_old = (player[0],player[1])
+                player[0] += 1 # right
+        elif nearest_food[4] > 0:
+            if nearest_food[-2] < 0:
+                player_old = (player[0],player[1])
+                player[1] -= 1 # up
+            elif nearest_food[-2] > 0:
+                player_old = (player[0],player[1])
+                player[1] += 1 # down
+        wrapping_and_history_update()
+        move_check()
+        #time.sleep(0.1)
+
+def init(starting_food):
+    global screen_width, screen_height, grid, history, food, food_move_gain, original_moves, moves, colour_change, history_colour, player
+    seed_gen = randrange(sys.maxsize) 
+    print("Seed was:", seed_gen)
+    seed(seed_gen) # same level every time
+    grid = {} #keys are the co ords, values are the type of cell
+    history = {}
+    total_food = starting_food
+    while total_food > 0:
+        x,y = randint(0,grid_width-1),randint(0,grid_height-1)
+        if (x,y) not in grid:
+            grid[(x,y)] = 1
+            total_food -= 1
+    food = 0
+    food_move_gain = 4
+    original_moves = 15
+    moves = original_moves
+    history_colour = [128.0,128.0,128.0]
+    colour_change = (128/moves)
+    player = [round(grid_width/2)-1,round(grid_height/2)-1]
+    player_old = [0,0]
+    add_history()
+    fill_grid()
+    draw_grid_lines()
+    pygame.display.update() #first draw
+    
+pygame.init()
+info = pygame.display.Info() # You have to call this before pygame.display.set_mode()
+screen_width,screen_height = info.current_w,info.current_h
+print(screen_width,screen_height)
+grid_size = 40 #automate the process of picking this number so it perfectly fits
+grid_height = round(screen_height/grid_size)
+grid_width = round(screen_width/grid_size)
+width, height = grid_size*grid_width, grid_size*grid_height
+print(grid_width,grid_height)
+surface = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+
+init(82)
+
+SONG_END = pygame.USEREVENT + 1
+
+pygame.mixer.music.set_endevent(SONG_END)
+pygame.mixer.music.load('intro.ogg')
+pygame.mixer.music.play()
+
+bot = False
+
+running = True
+
 while running:
-    key_check()
+    if bot:
+        choose_move()
+    for event in pygame.event.get():
+        if event.type == SONG_END:
+            pygame.mixer.music.load('song.ogg')
+            pygame.mixer.music.play(-1)
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                running = False
+                break
+            elif not bot:
+                key_check(event)
+    if game_over():
+        init(82)
+        #running = False
+        
 
 print("Food Collected: {}".format(food))
 
